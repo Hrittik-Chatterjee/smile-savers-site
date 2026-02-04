631eb9b061df7920c535a2b4b8ce110f12abb75a
hrittik-chatterjee <hrittik0chatterjee@gmail.com>
2026-02-05 02:49:03 +0600

    conflict resolved story blok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 import robotsTxt from 'astro-robots-txt';
-import { storyblok } from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,16 +13,6 @@ export default defineConfig({
         sitemap(),
         partytown({ config: { forward: ['dataLayer.push'] } }),
         robotsTxt(),
-        storyblok({
-            accessToken: "audR8TSb4Z4r1Qw5qMebiQtt",
-            apiOptions: {
-                region: "eu",
-            },
-            components: {
-                page: "storyblok/Page",
-                feature: "storyblok/Feature",
-            },
-        }),
     ],
     prefetch: {
         prefetchAll: true,
