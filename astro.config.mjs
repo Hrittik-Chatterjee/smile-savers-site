84717936c852adfec9698990bb6754eca7b7c8bd
hrittik-chatterjee <hrittik0chatterjee@gmail.com>
2026-02-05 21:54:11 +0600

    storyblok added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,26 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 import robotsTxt from 'astro-robots-txt';
+import { storyblok } from '@storyblok/astro';
+import { loadEnv } from 'vite';
+
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://smilesavers.dental',
     output: 'static',
     integrations: [
+        storyblok({
+            accessToken: env.STORYBLOK_TOKEN,
+            components: {
+                // Add your Storyblok components here, e.g.:
+                // page: 'storyblok/Page',
+            },
+            apiOptions: {
+                region: 'eu',
+            },
+        }),
         sitemap(),
         partytown({ config: { forward: ['dataLayer.push'] } }),
         robotsTxt(),
